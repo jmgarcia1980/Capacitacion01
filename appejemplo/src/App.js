abbba8ddd41bbea9c917b58d269bc78c6b01a8b9
jmgarcia1980 <mail.jgarcia@gmail.com>
2020-01-21 17:51:50 +0000

    Update priemra de la tarde.. JM

--- appejemplo/src/App.js
+++ appejemplo/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Home from './page';
-
+import About from './page/About'
+import Dashboard from './page/Dashboard';
 
 function App() {
   return (
@@ -14,12 +15,24 @@ function App() {
          <li>
            <Link to="/">Home</Link>
          </li>
+         <li>
+           <Link to="/about">About</Link>
+         </li>
+         <li>
+           <Link to="/dashboard">Dashboard</Link>
+         </li>
        </ul>
 
        <hr />
 
          <Route exact path="/">
            <Home />
+         </Route>
+         <Route path="/about">
+           <About />
+         </Route> 
+         <Route path="/dashboard">
+           <Dashboard />
          </Route>
      
      </div>
