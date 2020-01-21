ccb7ead30686ecabd8c824d209b4d36b627e5612
jmgarcia1980 <mail.jgarcia@gmail.com>
2020-01-21 14:53:00 +0000

    Subida 2 del ejemplo JM

--- appejemplo/src/App.js
+++ appejemplo/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './page';
+
 
 function App() {
   return (
-    <h1>Hola</h1>
-  );
+
+<Router>
+     <div>
+       <ul>
+         <li>
+           <Link to="/">Home</Link>
+         </li>
+       </ul>
+
+       <hr />
+
+         <Route exact path="/">
+           <Home />
+         </Route>
+     
+     </div>
+   </Router>
+
+ );
 }
 
 export default App;
